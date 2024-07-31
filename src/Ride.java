60b017a8aa8e1a14bb167c55f818f20eb56cb1ae
Mahela <lenken125@gmail.com>
2024-08-01 01:44:45 +1000

    twelfth commit

--- src/Ride.java
+++ src/Ride.java
@@ -1,7 +1,21 @@
-public class Ride {
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+public class Ride implements RideInterface {
     private String rideName;
     private int minAge;
     private Employee operator;
+    private Queue<Visitor> visitorQueue = new LinkedList<>();
+    private LinkedList<String> rideHistory = new LinkedList<>();
+
+
+
+    public Ride(String rideName, int minAge, Employee operator){
+        this.setRideName(rideName);
+        this.setMinAge(minAge);
+        this.setOperator(operator);
+    }
 
 
     public String Details() {
@@ -34,5 +48,52 @@ public class Ride {
 
     public void assignOperator(Employee operator) {
         this.operator = operator;
+    }
+
+    @Override
+    public void addVisitorToQueue(Visitor visitor) {
+        if (visitor.getAge() >= minAge) {
+            visitorQueue.add(visitor);
+            System.out.println("Visitor " + visitor.getName() + " has been added to the queue.");
+        } else {
+            System.out.println("Visitor " + visitor.getName() + " is too young for the ride.");
+        }
+    }
+
+    @Override
+    public void removeVisitorFromQueue(Visitor visitor) {
+        if (visitorQueue.remove(visitor)) {
+            System.out.println("Visitor " + visitor.getName() + " has been removed from the queue.");
+        } else {
+            System.out.println("Visitor " + visitor.getName() + " is not in the queue.");
+        }
+    }
+
+    @Override
+    public void printQueue() {
+        System.out.println("Visitors in the queue for " + rideName + ":");
+        for (Visitor visitor : visitorQueue) {
+            System.out.println(visitor.Details());
+        }
+    }
+
+    @Override
+    public void runOneCycle() {
+        if (!visitorQueue.isEmpty()) {
+            Visitor visitor = visitorQueue.poll();
+            String historyEntry = "Visitor " + visitor.getName() + " enjoyed the ride " + rideName;
+            rideHistory.add(historyEntry);
+            System.out.println(historyEntry);
+        } else {
+            System.out.println("No visitors in the queue for the ride.");
+        }
+    }
+
+    @Override
+    public void printRideHistory() {
+        System.out.println("Ride history for " + rideName + ":");
+        for (String entry : rideHistory) {
+            System.out.println(entry);
+        }
     }
 }
