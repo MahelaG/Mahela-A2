f23d794df85c83560b3312780ee6bda8ea1fe468
Mahela <lenken125@gmail.com>
2024-07-31 22:29:18 +1000

    ten commit

--- src/AssignmentTwo.java
+++ src/AssignmentTwo.java
@@ -1,4 +1,28 @@
 public class AssignmentTwo {
     public static void main(String[] args) {
+
+    }
+    public void partThree() {
+        // Implementation for part three
+    }
+
+    public void partFourA() {
+        // Implementation for part four A
+    }
+
+    public void partFourB() {
+        // Implementation for part four B
+    }
+
+    public void partFive() {
+        // Implementation for part five
+    }
+
+    public void partSix() {
+        // Implementation for part six
+    }
+
+    public void partSeven() {
+        // Implementation for part seven
     }
 }
